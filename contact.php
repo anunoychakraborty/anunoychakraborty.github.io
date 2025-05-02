ec8c584d4fd828eed32c54db9718384f8efd9f90
anunoychakraborty <anunoymaths2017@gmail.com>
2025-05-02 04:43:45 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE FOLLOWING TWO LINES:
-    $email_to = "you@yourdomain.com";
+    $email_to = "anunoymaths2017@gmail.com";
     $email_subject = "New form submissions";
 
     function problem($error)
